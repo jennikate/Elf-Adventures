84ffd4d0abb24b79005375a2d3d87205565f16e7
JenniKate Wallace <jennikate@gmail.com>
2019-10-31 14:37:03 +0000

    wall objects

--- scripts/app.js
+++ scripts/app.js
@@ -20,91 +20,127 @@ function pacnam() {
 
   // WALLS
   // CAN PROBABLY MAKE A FUNCTION FOR THE LOOP THAT CALLS THE ARRAYS, PERHAPS : THINK ABOUT IT
-  //create wall array for index of top walls (class wall-top, sets border=top)
-  const wallsTop = [
-    0,1,2,3,4,5,6,7,8,9,
-    11,13,16,18,
-    21,23,26,28,
-    31,34,35,38,
-    40,41,43,46, 48,49,
-    50,51,58,59,
-    64,65,
-    74,75,
-    81,83,86,88,
-    94,95]
-  for (let i = 0; i < wallsTop.length; i++) {
-    const cellId = '#cell' + wallsTop[i]
+
+  //create array of objects to say which borders have walls
+  const walls = [
+    { cellId: 0,  top: true, right: false, bottom: false, left: true },
+    { cellId: 1,  top: true, right: false, bottom: true, left: false },
+    { cellId: 2,  top: true, right: false, bottom: false, left: false },
+    { cellId: 3,  top: true, right: false, bottom: true, left: false },
+    { cellId: 4,  top: true, right: true, bottom: false, left: false },
+    { cellId: 5,  top: true, right: false, bottom: false, left: true },
+    { cellId: 6,  top: true, right: false, bottom: true, left: false },
+    { cellId: 7,  top: true, right: false, bottom: false, left: false },
+    { cellId: 8,  top: true, right: false, bottom: true, left: false },
+    { cellId: 9,  top: true, right: true, bottom: false, left: false },
+
+    { cellId: 10,  top: false, right: true, bottom: false, left: true },
+    { cellId: 11,  top: true, right: true, bottom: true, left: true },
+    { cellId: 12,  top: false, right: true, bottom: false, left: true },
+    { cellId: 13,  top: true, right: true, bottom: true, left: true },
+    { cellId: 14,  top: false, right: true, bottom: false, left: true },
+    { cellId: 15,  top: false, right: true, bottom: false, left: true },
+    { cellId: 16,  top: true, right: true, bottom: true, left: true },
+    { cellId: 17,  top: false, right: true, bottom: false, left: true },
+    { cellId: 18,  top: true, right: true, bottom: true, left: true },
+    { cellId: 19,  top: false, right: true, bottom: false, left: true },
+
+  ]
+
+  for (let i = 0; i < walls.length; i++) {
+    const wallKey = Object.keys(walls[i])
+    const wallValue = Object.values(walls[i])
+    const cellId = '#cell' + walls[i].cellId
+    // console.log(cellId)
     const cellLocation = document.querySelector(cellId)
-    cellLocation.classList.add('wall-top')
+    console.log(cellLocation)
+    //check and add border
+    if ( walls[i].top === true ) {
+      cellLocation.classList.add('wall-top')
+    }
+    if ( walls[i].right === true ) {
+      cellLocation.classList.add('wall-right')
+    }
+    if ( walls[i].bottom === true ) {
+      cellLocation.classList.add('wall-bottom')
+    }
+    if ( walls[i].left === true ) {
+      cellLocation.classList.add('wall-left')
+    }
+
+
   }
 
-  //create wall array for index of left walls (class wall-left, sets border-left)
-  const wallsLeft = [
-    0,5,
-    10,11,12,13,14,15,16,17,18,19,
-    20,
-    30,33,35,37,
-    40,42,43,47,48,
-    54,56,
-    60,63,67,
-    70,75,
-    80,82,88,
-    90]
-  for (let i = 0; i < wallsLeft.length; i++) {
-    const cellId = '#cell' + wallsLeft[i]
-    const cellLocation = document.querySelector(cellId)
-    cellLocation.classList.add('wall-left')
-  }
+  // //create wall array for index of top walls (class wall-top, sets border=top)
+  // const wallsTop = [
+  //   0,1,2,3,4,5,6,7,8,9,
+  //   11,13,16,18,
+  //   21,23,26,28,
+  //   31,34,35,38,
+  //   40,41,43,46, 48,49,
+  //   50,51,58,59,
+  //   64,65,
+  //   74,75,
+  //   81,83,86,88,
+  //   94,95]
+  // for (let i = 0; i < wallsTop.length; i++) {
+  //   const cellId = '#cell' + wallsTop[i]
+  //   const cellLocation = document.querySelector(cellId)
+  //   cellLocation.classList.add('wall-top')
+  // }
 
-  //create wall array for index of right walls (class wall-right, sets border-right)
-  const wallsRight = [
-    4,9,
-    10,11,12,13,14,15,16,17,18,19,
-    29,
-    32,34,36,39,
-    41,42,46,47,49,
-    53,55,
-    62,66,69,
-    74,79,
-    81,87,89,
-    99]
-  for (let i = 0; i < wallsRight.length; i++) {
-    const cellId = '#cell' + wallsRight[i]
-    const cellLocation = document.querySelector(cellId)
-    cellLocation.classList.add('wall-right')
-  }
-  //create wall array for index of bottom walls (class wall-bottom, sets border-bottom)
-  const wallsBottom = [
-    1,3,6,8,
-    11,13,16,18,
-    21,24,25,28,
-    30,31,33,36,38,39,
-    40,41,48,49,
-    54,55,
-    64,65,
-    71,73,76,78,
-    84,85,
-    90,91,92,93,94,95,96,97,98,99]
-  for (let i = 0; i < wallsBottom.length; i++) {
-    const cellId = '#cell' + wallsBottom[i]
-    const cellLocation = document.querySelector(cellId)
-    cellLocation.classList.add('wall-bottom')
-  }
+  // //create wall array for index of left walls (class wall-left, sets border-left)
+  // const wallsLeft = [
+  //   0,5,
+  //   10,11,12,13,14,15,16,17,18,19,
+  //   20,
+  //   30,33,35,37,
+  //   40,42,43,47,48,
+  //   54,56,
+  //   60,63,67,
+  //   70,75,
+  //   80,82,88,
+  //   90]
+  // for (let i = 0; i < wallsLeft.length; i++) {
+  //   const cellId = '#cell' + wallsLeft[i]
+  //   const cellLocation = document.querySelector(cellId)
+  //   cellLocation.classList.add('wall-left')
+  // }
 
-  //(create game board)
-  // set width to level number * 5 [might change 5 to another number later]
-  // create array of (wall positions)
-  // loop to (width ** 2)
-  // create div with class of cell
-  // if div[index] === wall position then set class wall
-  // attach to gameboard div
-
-  //(wall positions)
-  // *** needs thought on what the grid would look like and then translate that back to linear indexes ***
-  // *** thinking static for MVP and designed for up to level [x] ***
-  // array of arrays so can see horizontal and vertical
-  // create array of wall positions with list of indexes for where the wall goes
-
+  // //create wall array for index of right walls (class wall-right, sets border-right)
+  // const wallsRight = [
+  //   4,9,
+  //   10,11,12,13,14,15,16,17,18,19,
+  //   29,
+  //   32,34,36,39,
+  //   41,42,46,47,49,
+  //   53,55,
+  //   62,66,69,
+  //   74,79,
+  //   81,87,89,
+  //   99]
+  // for (let i = 0; i < wallsRight.length; i++) {
+  //   const cellId = '#cell' + wallsRight[i]
+  //   const cellLocation = document.querySelector(cellId)
+  //   cellLocation.classList.add('wall-right')
+  // }
+  // //create wall array for index of bottom walls (class wall-bottom, sets border-bottom)
+  // const wallsBottom = [
+  //   1,3,6,8,
+  //   11,13,16,18,
+  //   21,24,25,28,
+  //   30,31,33,36,38,39,
+  //   40,41,48,49,
+  //   54,55,
+  //   64,65,
+  //   71,73,76,78,
+  //   84,85,
+  //   90,91,92,93,94,95,96,97,98,99]
+  // for (let i = 0; i < wallsBottom.length; i++) {
+  //   const cellId = '#cell' + wallsBottom[i]
+  //   const cellLocation = document.querySelector(cellId)
+  //   cellLocation.classList.add('wall-bottom')
+  // }
 
 
 }
